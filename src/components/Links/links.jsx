795afc0f49cf05d78819426c725e67ef15e61a88
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-02-22 18:45:21 +0000

    added all content and profile pic

---
+++ src/components/Links/links.jsx
@@ -0,0 +1,12 @@
+const links = () => {
+  return (
+    <div>
+      <div>Github</div>
+      <div>LinkedIn</div>
+      <div>Twitter</div>
+      <div>Frontend Mentor</div>
+    </div>
+  );
+};
+
+export default links;
---
+++ src/components/Personal/personal.jsx
@@ -0,0 +1,16 @@
+const personal = () => {
+  return (
+    <div>
+      <div>
+        <img src='../../../public/headshot.png' alt='profile picture' />
+      </div>
+      <div>
+        <h1>Lawrence Narh-Lawson</h1>
+        <p>London, United Kingdom</p>
+        <p>Frontend Developer</p>
+      </div>
+    </div>
+  );
+};
+
+export default personal;
